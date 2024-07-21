244a2f4511a875e7cd7e9801461ec690cf8c7cde
Ricardo Alcala Vasquez <ricardo.alcala1@gmail.com>
2024-07-21 11:49:12 -0500

    Core proyect and migrations

---
+++ employees/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class Agency(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'agency'
+
+
+class Employee(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    agency = models.ForeignKey(Agency, models.DO_NOTHING)
+    profession = models.ForeignKey('Profession', models.DO_NOTHING)
+    gender = models.ForeignKey('Gender', models.DO_NOTHING)
+    ethnicity = models.ForeignKey('Ethnicity', models.DO_NOTHING)
+    monthly_salary = models.DecimalField(max_digits=16, decimal_places=2)
+    md5 = models.CharField(unique=True, max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'employee'
+
+
+class Ethnicity(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'ethnicity'
+
+
+class Gender(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'gender'
+
+
+class Profession(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'profession'
